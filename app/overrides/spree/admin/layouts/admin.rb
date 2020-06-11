5e1438a4b5553ce9b36014fd59616b727588d3ad
Satyakam Pandya <satyakamp@nividata.com>
2020-06-11 15:30:52 +0530

    Updated tab to make it selected

--- app/overrides/spree/admin/layouts/admin.rb
+++ app/overrides/spree/admin/layouts/admin.rb
@@ -5,12 +5,12 @@ Deface::Override.new(
   text:       <<-HTML
                 <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
                   <ul class="nav nav-sidebar">
-                    <%= tab "Authors", url: admin_vendors_path, icon: 'money' %>
+                    <%= tab "Authors", :vendors, url: admin_vendors_path, icon: 'money' %>
                   </ul>
                 <% end %>
                 <% if defined?(current_spree_vendor) && current_spree_vendor %>
                   <ul class="nav nav-sidebar">
-                    <%= tab "Profile", url: admin_profile_path, icon: 'user' %>
+                    <%= tab "Profile", :vendor_settings, url: admin_profile_path, icon: 'user' %>
                   </ul>
                 <% end %>
               HTML
