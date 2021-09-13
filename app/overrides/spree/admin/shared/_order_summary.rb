f709fd312c530586ec3089e20e3292670f26bb64
Satyakam Pandya <satyakamp@nividata.com>
2021-09-13 18:12:53 +0530

    Commented/Updated overridden snippets

--- app/overrides/spree/admin/shared/_order_summary.rb
+++ app/overrides/spree/admin/shared/_order_summary.rb
@@ -1,23 +1,23 @@
-Deface::Override.new(
-    virtual_path: 'spree/admin/shared/_order_summary',
-    name: 'display_vendor_subtotal',
-    replace: "erb[loud]:contains('@order.display_item_total.to_html')",
-    text: "<%= current_spree_vendor ? @order.display_vendor_subtotal(current_spree_vendor).to_html : @order.display_item_total.to_html %>"
-)
+# Deface::Override.new(
+#     virtual_path: 'spree/admin/shared/_order_summary',
+#     name: 'display_vendor_subtotal',
+#     replace: "erb[loud]:contains('@order.display_item_total.to_html')",
+#     text: "<%= current_spree_vendor ? @order.display_vendor_subtotal(current_spree_vendor).to_html : @order.display_item_total.to_html %>"
+# )
 
-Deface::Override.new(
-    virtual_path: 'spree/admin/shared/_order_summary',
-    name: 'display_vendor_ship_total',
-    replace: "erb[loud]:contains('@order.display_ship_total.to_html')",
-    text: "<%= current_spree_vendor ? @order.display_vendor_ship_total(current_spree_vendor).to_html : @order.display_ship_total.to_html %>"
-)
+# Deface::Override.new(
+#     virtual_path: 'spree/admin/shared/_order_summary',
+#     name: 'display_vendor_ship_total',
+#     replace: "erb[loud]:contains('@order.display_ship_total.to_html')",
+#     text: "<%= current_spree_vendor ? @order.display_vendor_ship_total(current_spree_vendor).to_html : @order.display_ship_total.to_html %>"
+# )
 
-Deface::Override.new(
-    virtual_path: 'spree/admin/shared/_order_summary',
-    name: 'display_vendor_total',
-    replace: "erb[loud]:contains('@order.display_total.to_html')",
-    text: "<%= current_spree_vendor ? @order.display_vendor_total(current_spree_vendor).to_html : @order.display_total.to_html %>"
-)
+# Deface::Override.new(
+#     virtual_path: 'spree/admin/shared/_order_summary',
+#     name: 'display_vendor_total',
+#     replace: "erb[loud]:contains('@order.display_total.to_html')",
+#     text: "<%= current_spree_vendor ? @order.display_vendor_total(current_spree_vendor).to_html : @order.display_total.to_html %>"
+# )
 Deface::Override.new(
     virtual_path: 'spree/admin/shared/_order_summary',
     name: 'display_commission',
@@ -28,9 +28,7 @@ Deface::Override.new(
                 <strong><%= Spree.t('admin.commission') %>:</strong>
               </td>
               <td id='order_commission'>
-                <% if current_spree_vendor %>
-                  <%= @order.display_vendor_commission(current_spree_vendor) %>
-                <% elsif @order.commissions.present? %>
+                <% if @order.commissions.present? %>
                   <% @order.commissions.each do |vendor_commission| %>
                     <%= vendor_commission.vendor.name %> - <%= Spree::Money.new(vendor_commission.amount, {currency: @order.currency}) %> <br/>
                   <% end %>
